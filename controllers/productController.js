6e29f2bbfaacdf41242a953ca74fe4c26136b605
David Cladera <ddaudiosolutions@gmail.com>
2021-10-26 13:05:40 +0200

    cambios en subida de edicion productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,22 +13,25 @@ exports.crearProducto = async (req, res, next) => {
   try {
     //CREAR UN PRODUCTO
     const producto = new Producto(req.body);
-    
+
     //PARA SUBIR UNA SOLA IMAGEN
     // producto.images = {
     //   url: req.file.path,
     //   filename: req.file.filename,
     // };
-    
+
     //PARA SUBIR VARIAS IMAGENES
-    producto.images = req.files.map(f => ({url: f.path, filename: f.filename}))
-    console.log(producto.images)
+    producto.images = req.files.map((f) => ({
+      url: f.path,
+      filename: f.filename,
+    }));
+    console.log(producto.images);
     //GUARDAR EL CREADOR VIA JWT
     producto.author = req.user.id; //REACTIVAR AL TENER EL STATE DEL USUARIO
 
     //GUARDAMOS EL PROYECTO
     await producto.save();
-    console.log(producto)
+    console.log(producto);
 
     res.json(producto);
   } catch (error) {
@@ -39,7 +42,7 @@ exports.crearProducto = async (req, res, next) => {
 
 //OBTENER PRODUCTOS //TRABAJAMOS SIEMPRE QUE TRY CATCH PARA TENER MÁS SEGURIDAD Y CONTROL
 exports.obtenerProductos = async (req, res) => {
-  let busqueda = req.query.busqueda
+  let busqueda = req.query.busqueda;
   // console.log(busqueda);
   let busquedaValue = req.query.busqueda;
   //console.log(busquedaValue);
@@ -48,26 +51,26 @@ exports.obtenerProductos = async (req, res) => {
     //limit = 6;
   } else {
     busquedaValue = { categoria: busqueda };
-   // limit = null;
+    // limit = null;
   }
 
   try {
     const PAGE_SIZE = 8;
     const page = parseInt(req.query.page || "0");
-    const totalProductos = await Producto.countDocuments( busquedaValue );
+    const totalProductos = await Producto.countDocuments(busquedaValue);
     const totalPages = Math.ceil(totalProductos / PAGE_SIZE);
-    
+
     const prodAll = await Producto.find(busquedaValue)
       .limit(PAGE_SIZE)
       .skip(PAGE_SIZE * page)
       .sort({ creado: -1 })
-      .populate({path: 'author', select: 'nombre direccion telefono email'})
-      
-      //console.log(prodAll.author.nombre);
+      .populate({ path: "author", select: "nombre direccion telefono email" });
+
+    //console.log(prodAll.author.nombre);
     res.json({ prodAll, totalProductos, totalPages });
     // const prodAll = await Producto.find(busquedaValue).sort({ creado: -1 }).limit(limit);
     // res.json({ prodAll });
-     //console.log(totalProductos, totalPages, prodAll);
+    //console.log(totalProductos, totalPages, prodAll);
   } catch (error) {
     //console.log(error);
     res.status(500).send({ error: "Hubo un Error" });
@@ -76,17 +79,19 @@ exports.obtenerProductos = async (req, res) => {
 
 exports.obtenerProductosUser = async (req, res) => {
   try {
-     const PAGE_SIZE = 8;
+    const PAGE_SIZE = 8;
     // const page = parseInt(req.query.page || "0");
-    const totalProductosUs = await Producto.countDocuments({ author: req.user.id });
+    const totalProductosUs = await Producto.countDocuments({
+      author: req.user.id,
+    });
     const totalPagesUs = Math.ceil(totalProductosUs / PAGE_SIZE);
-    const prodUser = await Producto.find({ author: req.user.id })      
-    // .limit(PAGE_SIZE)  
-    // .skip(PAGE_SIZE * page)      
-    .sort({
-      creado: -1,
-    })
-    .populate({path: 'author', select: 'nombre direccion telefono email'});
+    const prodUser = await Producto.find({ author: req.user.id })
+      // .limit(PAGE_SIZE)
+      // .skip(PAGE_SIZE * page)
+      .sort({
+        creado: -1,
+      })
+      .populate({ path: "author", select: "nombre direccion telefono email" });
     //console.log(prodUser, totalProductosUs, totalPagesUs);
     res.json({ prodUser, totalProductosUs, totalPagesUs });
   } catch (error) {
@@ -101,9 +106,9 @@ exports.obtenerProductosUser = async (req, res) => {
 //     const page = parseInt(req.query.page || "0");
 //     const totalProductosUs = await Producto.countDocuments({ author: req.user.id });
 //     const totalPagesUs = Math.ceil(totalProductosUs / PAGE_SIZE);
-//     const prodUser = await Producto.find({ author: req.user.id })      
-//     .limit(PAGE_SIZE)  
-//     .skip(PAGE_SIZE * page)      
+//     const prodUser = await Producto.find({ author: req.user.id })
+//     .limit(PAGE_SIZE)
+//     .skip(PAGE_SIZE * page)
 //     .sort({
 //       creado: -1,
 //     });
@@ -116,22 +121,24 @@ exports.obtenerProductosUser = async (req, res) => {
 // };
 
 exports.obtenerProductosAuthor = async (req, res) => {
- // const authorid = req.query.authorid
+  // const authorid = req.query.authorid
   //const authorId = req.params
   //console.log(authorId)
   try {
-     //const PAGE_SIZE = 8;
+    //const PAGE_SIZE = 8;
     // const page = parseInt(req.query.page || "0");
-    const totalProductosAuth = await Producto.countDocuments({ author: req.params.id});
+    const totalProductosAuth = await Producto.countDocuments({
+      author: req.params.id,
+    });
     //const totalPagesUs = Math.ceil(totalProductosUs / PAGE_SIZE);
-    const prodAuth = await Producto.find({ author: req.params.id })      
-    // .limit(PAGE_SIZE)  
-    // .skip(PAGE_SIZE * page)      
-    .sort({
-      creado: -1,
-    })
-    .populate({path: 'author', select: 'nombre  email imagesAvatar '});
-    
+    const prodAuth = await Producto.find({ author: req.params.id })
+      // .limit(PAGE_SIZE)
+      // .skip(PAGE_SIZE * page)
+      .sort({
+        creado: -1,
+      })
+      .populate({ path: "author", select: "nombre  email imagesAvatar " });
+
     console.log(prodAuth, totalProductosAuth);
     res.send({ prodAuth, totalProductosAuth });
   } catch (error) {
@@ -143,10 +150,11 @@ exports.obtenerProductosAuthor = async (req, res) => {
 //OBTENER PRODUCTO POR ID //TRABAJAMOS SIEMPRE QUE TRY CATCH PARA TENER MÁS SEGURIDAD Y CONTROL
 exports.obtenerProductoId = async (req, res) => {
   try {
-    const productoId = await Producto.findById(req.params.id)
-      .populate({path: 'author', select: 'nombre direccion telefono email imagesAvatar'});
-      
-  
+    const productoId = await Producto.findById(req.params.id).populate({
+      path: "author",
+      select: "nombre direccion telefono email imagesAvatar",
+    });
+
     res.json({ productoId });
   } catch (error) {
     console.log(error);
@@ -158,7 +166,7 @@ exports.obtenerProductoId = async (req, res) => {
 exports.obtenerProductoEditar = async (req, res) => {
   try {
     const producto = await Producto.findById(req.params.id);
-   
+
     console.log("el producto :" + producto);
     res.json({ producto });
   } catch (error) {
@@ -168,19 +176,21 @@ exports.obtenerProductoEditar = async (req, res) => {
 };
 
 //EDITAR UN PRODUCTO
+
+
 exports.editarProductoUser = async (req, res, next) => {
   //REVISAR SI HAY ERRORES
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  console.log(req.body)
-  
+  console.log(req.body);
+  const { imagesDelete, title, categoria, subCategoria, price, description, contacto } = req.body;
 
   try {
     //   //REVISAR EL ID
     const productoTest = await Producto.findById(req.params.id);
-    
+
     //   //SI EL PRODUCTO EXISTE O NO!!!
     if (!productoTest) {
       console.log("hay un error en edicion");
@@ -192,26 +202,62 @@ exports.editarProductoUser = async (req, res, next) => {
       return res.status(401).json({ msg: "No Autorizado para Editar" });
     }
 
+    if (imagesDelete !== undefined) {
+      if (typeof imagesDelete === "string") {
+        cloudinary.uploader.destroy(imagesDelete, function (err, res) {
+          if (err) {
+            console.log(err);
+            return res.status(400).json({
+              ok: false,
+              menssage: "Error deleting file",
+              errors: err,
+            });
+          }
+          console.log(res);
+        });
+      } else {
+        for (let filename of imagesDelete) {
+          console.log(filename);
+          cloudinary.uploader.destroy(filename, function (err, res) {
+            if (err) {
+              console.log(err);
+              return res.status(400).json({
+                ok: false,
+                menssage: "Error deleting file",
+                errors: err,
+              });
+            }
+            console.log(res);
+          });
+        }
+      }
+    }
+
     //ACTUALIZAR PRODUCTO
     const producto = await Producto.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
+      req.params.id, 
+      {
+      '$pull': {"images":  {"filename": imagesDelete }}, 
+      '$set': {title, categoria, subCategoria, price, description, contacto}},     
+      {new: true}
     );
 
-    const images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    producto.images.push(...images)
-      console.log(producto.images)
-    // producto.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    // //producto.images.push(...images)
-    // await producto.save()
-   
-     await producto.save()
-     res.json({ producto });
+    
+    const images = req.files.map((f) => ({
+      url: f.path,
+      filename: f.filename,
+    }));
+
+    //if(producto.images === undefined) return null;
+    producto.images.push(...images);
+    console.log(producto.images);
+
+    await producto.save();
+    res.json({ producto });
   } catch (error) {
     console.log(error);
     res.status(500).send("Hubo un Error");
-    }
+  }
 };
 
 //
@@ -226,7 +272,7 @@ exports.eliminarProducto = async (req, res) => {
   try {
     //REVISAR EL ID
     let producto = await Producto.findById(req.params.id);
-    console.log(producto)
+    console.log(producto);
 
     //SI EL PRODUCTO EXISTE O NO!!!
     if (!producto) {
@@ -241,22 +287,19 @@ exports.eliminarProducto = async (req, res) => {
     //ELIMINAR EL PRODUCTO
     producto = await Producto.findByIdAndDelete(req.params.id);
     //ITERAMOS SOBRE LAS IMAGENES PARA TOMAR EL NOMBRE DE CADA IMAGEN Y BORRARLA EN CLOUDINARY
-    for(let images of producto.images){   
-       cloudinary.uploader.destroy(
-        images.filename,
-        function (err, res) {
-          if (err) {
-            console.log(err);
-            return res.status(400).json({
-              ok: false,
-              menssage: "Error deleting file",
-              errors: err,
-            });
-          }
-           console.log(res);
+    for (let images of producto.images) {
+      cloudinary.uploader.destroy(images.filename, function (err, res) {
+        if (err) {
+          console.log(err);
+          return res.status(400).json({
+            ok: false,
+            menssage: "Error deleting file",
+            errors: err,
+          });
         }
-      );}   
-    
+        console.log(res);
+      });
+    }
 
     res.json({ msg: "PRODUCTO ELIMINADO" });
   } catch (error) {
