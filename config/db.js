c3803cf9a697835b53bd6ef60f629de4401fb804
David_Cladera <dcladera@externos.itnow.es>
2023-03-16 13:30:28 +0100

    hotfix mongo

--- config/db.js
+++ config/db.js
@@ -4,13 +4,12 @@ require("dotenv").config({ path: "variables.env" });
 
 const conectarDB = async () => {
   try {
-   const conn = await mongoose.connect(process.env.DB_MONGO, {
+   await mongoose.connect(process.env.DB_MONGO, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn}`);
+    });   
     console.log("base de datos conectada");
   } catch (error) {
     console.log(error);
