f4be504ec9ec00b66a3192aa5187c3b98d01002c
David_Cladera <dcladera@externos.itnow.es>
2023-07-26 13:26:47 +0200

    secure comment

--- helpers/transporter.js
+++ helpers/transporter.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   /*  name: "hostgator", */
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false,
+  /* secure: false, */
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
