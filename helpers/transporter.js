9ab989f5e1695263101d648d5554bf7c70acc100
David_cladera <dcladera@externos.itnow.es>
2024-08-20 13:21:03 +0200

    fix nodemailer

--- helpers/transporter.js
+++ helpers/transporter.js
@@ -1,10 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  name: "gmail",
+  service: "gmail",
   /* host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT, */
-  /*  secure: true, */
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
